066ab762f617d69eeb407290637a6b8598785f85
Shin Than Thar Aung <133785479+khayasj@users.noreply.github.com>
2025-05-19 16:42:47 +0800

    Delete X-COVID Insight directory

--- X-COVID Insight/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();   // Remove all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to the login page
-header("Location: loginPage.html");
-exit();
-?>
